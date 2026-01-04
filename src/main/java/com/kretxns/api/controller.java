8e96bc2074a5b0c2ea09519aabc489ff376aed3b
unknown <otaviorufinno@gmail.com>
2026-01-04 19:23:33 -0300

    init spring boot project

---
+++ src/main/java/com/kretxns/api/controller.java
@@ -0,0 +1,4 @@
+package com.kretxns.api;
+
+public class controller {
+}
---
+++ src/test/java/com/kretxns/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kretxns.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
