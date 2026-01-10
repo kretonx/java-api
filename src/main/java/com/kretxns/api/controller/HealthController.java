03550c8a2c40b1b296fc863cb6137573ca07d34f
unknown <otaviorufinno@gmail.com>
2026-01-09 23:31:29 -0300

    wire accounts endpoint to service

--- src/main/java/com/kretxns/api/controller/HealthController.java
+++ src/main/java/com/kretxns/api/controller/HealthController.java
@@ -1,15 +1,29 @@
 package com.kretxns.api.controller;
 
+import com.kretxns.api.dto.CreateAccountRequest;
+import com.kretxns.api.model.Account;
+import com.kretxns.api.service.AccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    private final AccountService accountService;
+
+    public HealthController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @GetMapping("/health")
     public String health() {
         return "API rodando";
     }
 
+    @PostMapping("/accounts")
+    public Account createAccount(@RequestBody CreateAccountRequest request) {
+        return accountService.createAccount(request.titular);
+    }
 }
