248a24b0fbe08f02925b74a5232e98318437bbd8
unknown <otaviorufinno@gmail.com>
2026-01-10 21:59:09 -0300

    store accounts in memory and generate ids

--- src/main/java/com/kretxns/api/model/Account.java
+++ src/main/java/com/kretxns/api/model/Account.java
@@ -1,4 +1,7 @@
 package com.kretxns.api.model;
 
 public class Account {
+    public long id;
+    public String titular;
+    public double saldo;
 }
--- src/main/java/com/kretxns/api/service/AccountService.java
+++ src/main/java/com/kretxns/api/service/AccountService.java
@@ -1,4 +1,27 @@
-package service;
+package com.kretxns.api.service;
+
+import com.kretxns.api.model.Account;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+
 
 public class AccountService {
+    private final Map<Long, Account> accounts = new HashMap<>();
+    private long nextId =1;
+
+    public Account createAccount(String titular) {
+
+        Account account = new Account();
+        account.titular = titular;
+        account.saldo = 0;
+        account.id = nextId;
+        nextId++;
+
+        accounts.put(account.id, account);
+
+        return account;
+    }
 }
